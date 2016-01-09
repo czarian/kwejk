7ecd473f7e1ccb8ad46ca21a092531c3ec8d0928
Czarek <karasoi@poczta.onet.pl>
2016-01-09 17:21:27 +0100

    change mem index

--- app/models/mem.rb
+++ app/models/mem.rb
@@ -1,9 +1,9 @@
 class Mem < ActiveRecord::Base
-  after_validateion(on: :create) do 
+  after_validation(on: :create) do 
   	self.active = false
   end
 
-  end
+  validates :name, presence: true
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
