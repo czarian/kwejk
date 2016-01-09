06bfeb31b826de164ee1922be99b5717530e76b1
Czarek <karasoi@poczta.onet.pl>
2016-01-09 17:34:22 +0100

    krok 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :mems
+  resources :mems do
+    collection do
+      get 'my'
+      get 'inactive'
+    end
+  end
+  
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
